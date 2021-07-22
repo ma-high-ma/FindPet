59c3b2778fa249697313d46d8a76fb6760d94ceb
PARIJAT-HOME\mahim <mahima061998@gmail.com>
2021-07-23 02:37:25 +0530

    recyclerView completed and Data added from API

--- app/src/main/java/com/example/findpet/MainActivity.java
+++ app/src/main/java/com/example/findpet/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.findpet;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.LinearLayout;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.security.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +46,12 @@ public class MainActivity extends AppCompatActivity {
 
         initData();
         initRecyclerView();
-        
+
+        try {
+            populateAllPets();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -44,16 +70,86 @@ public class MainActivity extends AppCompatActivity {
     private void initData() {
 
         petList = new ArrayList<>();
-        petList.add(new ModelClass("ABC","1 month"));
-        petList.add(new ModelClass("DEF","2 months"));
-        petList.add(new ModelClass("GHI","3 months"));
-        petList.add(new ModelClass("JKL","1 month"));
-        petList.add(new ModelClass("MNO","5 months"));
-        petList.add(new ModelClass("ABC","1 month"));
-        petList.add(new ModelClass("DEF","2 months"));
-        petList.add(new ModelClass("GHI","3 months"));
-        petList.add(new ModelClass("JKL","1 month"));
-        petList.add(new ModelClass("MNO","5 months"));
+
 
     }
+
+    private void populateAllPets() throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://60d075407de0b20017108b89.mockapi.io/api/v1/animals";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response)  {
+
+                try {
+                    JSONArray jsonArray = new JSONArray(response.body().string());
+                    petList.clear();
+
+                    //Date current_time = Calendar.getInstance().getTime();
+                    String actualAge;
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        String time = jsonArray.getJSONObject(i).getString("bornAt");
+                        DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+                        Date actual_time = format.parse(time);
+
+                        long timestamp = actual_time.getTime();
+
+                        int elapsedTime = (int)(System.currentTimeMillis()/60000 - timestamp/60000);
+                        Log.d("Hi",elapsedTime+"");
+                        long elapsedMonths = (elapsedTime)/(24*30*60);
+
+                        long elapsedDays = (elapsedTime)/(24*60);
+                        long elapsedYears = (elapsedTime)/(24*365*60);
+                        long elapsedHours = (elapsedTime)/(60);
+                        Log.d("Hi",(elapsedTime)+"");
+
+                        if ( elapsedYears > 0)
+                        {
+                            actualAge = elapsedYears + " Years";
+                        }
+                        else if (elapsedMonths > 0)
+                        {
+                            actualAge = elapsedMonths + " Months";
+                        }
+                        else if (elapsedDays > 0)
+                        {
+                            actualAge = elapsedDays + " Days";
+                        }
+                        else
+                        {
+                            actualAge = elapsedHours + " Hours";
+                        }
+
+                        petList.add(new ModelClass(jsonArray.getJSONObject(i).getString("name"), actualAge+""));
+
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                } catch (JSONException | IOException | ParseException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.e("Juhi", "Error");
+            }
+        });
+    }
+
+
 }
